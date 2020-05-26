8b3ff86a61ca1adb0323ce35fa63b29dec45b090
njenn001 <njenn001@odu.edu>
2020-05-26 12:23:37 -0400

    Initial webpage repo

---
+++ scripts/pose.js
@@ -0,0 +1,12 @@
+class Rep {
+	constructor(){
+		this.top = 0; 
+		this.bottoms = []; 
+		this.uRom = 0; 
+		this.dRom = 0; 
+		this.uDur = 0; 
+		this.dDur = 0; 
+		this.uVel = 0; 
+		this.dVel = 0; 
+	}
+}
